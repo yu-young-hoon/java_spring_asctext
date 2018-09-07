0728a85a54cb10aced3b54b1730df8947d665b7c
yyh <dbtlek@gmauil.com>
2018-09-07 10:50:09 +0900

    - Added jsp, controller

--- src/main/java/com/yh/asctext/controller/IndexController.java
+++ src/main/java/com/yh/asctext/controller/IndexController.java
@@ -1,16 +1,47 @@
 package com.yh.asctext.controller;
 
 
+import com.yh.asctext.domain.ASCText;
+import com.yh.asctext.service.SolutionService;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class IndexController {
     private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+    @Autowired
+    private SolutionService solutionService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView Index(HttpServletRequest request
+            , HttpServletResponse response) {
+        ModelAndView mav = new ModelAndView();
+        logger.info("aa");
+        mav.setViewName("index");
+        return mav;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST, produces = "application/text; charset=utf8")
+    public ModelAndView Solution(HttpServletRequest request
+            , HttpServletResponse response) {
+        ModelAndView mav = new ModelAndView();
+
+        String text = request.getParameter("text");
+        String unit = request.getParameter("unit");
+        ASCText ascText = solutionService.Sort(text, Integer.parseInt(unit));
+
+        mav.setViewName("index");
+        mav.addObject("result", ascText);
+
+        return mav;
+    }
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,9 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<% request.setCharacterEncoding("UTF-8"); %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ko">
 <head>
-    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>asctext</title>
 </head>
 <body>
+    <form method="post">
+        <div>
+            <h1>입력</h1>
+            <textarea name="text"><c:out value="${result.originText}"></c:out></textarea>
+        </div>
+        <div>
+            <h1>출력묶음단위</h1>
+            <input name="unit" value="<c:out value="${result.outUnit}"></c:out>"/>
+        </div>
+        <input type="submit" value="출력"/>
+    </form>
+    <div>
+        <h1>몫</h1>
+        <div>
+            <c:out value="${result.resultText}"/>
+        </div>
+    </div>
+    <div>
+        <h1>나머지</h1>
+        <di>
+            <c:out value="${result.tailText}"/>
+        </div>
+    </div>
 </body>
 </html>
